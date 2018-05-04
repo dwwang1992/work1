35d45507d5bd00b90507e3bfa0fd84c289a9f073
dwwang1992 <dwwang1992@126.com>
2018-05-04 16:56:12 +0800

    push to github work1 first

--- dir1/second.py
+++ dir1/second.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 # author: WangDaWei
 
-for i in range(10):
-    print('hello'+str(i))
+import pymysql
 
+conn = pymysql.connect(host='118.190.209.102', port=5700, user='root', passwd='123456', charset='utf8')
+
+cursor = conn.cursor()
+sql = "show databases"
+cursor.execute(sql)
+result = cursor.fetchall()
+print(result)
