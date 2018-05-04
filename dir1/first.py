2f9855950d5c38d7e312ed5b301e3502685d9d76
dwwang1992 <dwwang1992@126.com>
2018-05-04 16:28:15 +0800

    push to github work1 first

--- dir1/first.py
+++ dir1/first.py
@@ -2,4 +2,19 @@
 # -*- coding: utf-8 -*-
 # author: WangDaWei
 
-print('hello')
+
+def func(x):
+    if x == 1:
+        return 1
+    else:
+        return x * func(x - 1)
+
+
+def main(wid):
+    print('hello,everybody!')
+    print(func(wid))
+
+
+if __name__ == '__main__':
+    main(10)
+    print(1*2*3*4*5*6*7*8*9*10)
